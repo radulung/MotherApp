f651f909158a09e7b16761771462441f4a83de69
radu.lung <radu.lung@tss-yonder.com>
2021-03-02 22:41:53 +0200

    Referenciate the class libraries and use the implemented operations

--- MotherApp/Program.cs
+++ MotherApp/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Submodule1;
+using Submodule2;
+using System;
 
 namespace MotherApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            AddOperation add = new AddOperation();
+            var addResult = add.Execute(2, 5);
+
+            SubstractOperation subsctract = new SubstractOperation();
+            var substractResult = subsctract.Execute(5, 2);
+
+            Console.WriteLine($"Add result: {addResult}");
+            Console.WriteLine($"Substract result: {substractResult}");
+            Console.ReadKey();
         }
     }
 }
